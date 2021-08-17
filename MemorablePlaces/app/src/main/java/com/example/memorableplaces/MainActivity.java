21f539dc54478e7ef698bc8edac3d2983ff886c9
Cristhian Cruz <c.cruzcoro73@gmail.com>
2021-08-16 19:35:13 -0500

    Memorable Places app finished

--- MemorablePlaces/app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ MemorablePlaces/app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.memorableplaces;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -20,37 +13,14 @@ import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-
-class MarkerInfo implements Serializable {
-    String title;
-    Location location;
-
-    public MarkerInfo(String title, Location location) {
-        this.title = title;
-        this.location = location;
-    }
-}
 
 public class MainActivity extends AppCompatActivity {
     ListView placesListView;
-    ArrayList<String> placeList;
-    ArrayList<Location> placeLocations;
-    LocationManager locationManager;
-    LocationListener locationListener;
+    static ArrayList<String> markerTitles = new ArrayList<>();
+    static ArrayList<MarkerInfo> markers = new ArrayList<>();
+    static ArrayAdapter<String> placesAdapter;
     MarkerInfo markerInfo;
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,50 +28,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         placesListView = (ListView) findViewById(R.id.placesListView);
-        placeList = new ArrayList<>();
-        placeList.add("Add a new place...");
-        placeLocations = new ArrayList<>();
-        placeLocations.add(null);
-
-        //Setting of location service
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                placeLocations.set(0, location);
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-        };
+        markerTitles.add("Add a new place...");
 
         // Settings of the list view
-        ArrayAdapter<String> placesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, placeList);
+        placesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, markerTitles);
         placesListView.setAdapter(placesAdapter);
+
         placesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 if(position == 0) {
-                    markerInfo = new MarkerInfo("You are here", placeLocations.get(0));
+                    markerInfo = new MarkerInfo("You are here", null);
                 }
                 else {
-                    markerInfo = new MarkerInfo(placeList.get(position), placeLocations.get(position));
+                    markerInfo = markers.get(position - 1);
                 }
-                intent.putExtra("markerInfo", markerInfo);
+                intent.putExtra("MarkerInfo", markerInfo);
                 startActivity(intent);
             }
         });
-
-        //Setting location permissions
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-            placeLocations.set(0, locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
-        }
-        else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
     }
+}
+
+
+class MarkerInfo implements Parcelable {
+    private String title;
+    private LatLng coordinates;
+
+    public MarkerInfo(String title, LatLng coordinates) {
+        this.title = title;
+        this.coordinates = coordinates;
+    }
+
+    protected MarkerInfo(Parcel in) {
+        title = in.readString();
+        coordinates = in.readParcelable(LatLng.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeParcelable(coordinates, flags);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LatLng getCoordinates() {
+        return coordinates;
+    }
+
+    public static final Creator<MarkerInfo> CREATOR = new Creator<MarkerInfo>() {
+        @Override
+        public MarkerInfo createFromParcel(Parcel in) {
+            return new MarkerInfo(in);
+        }
+
+        @Override
+        public MarkerInfo[] newArray(int size) {
+            return new MarkerInfo[size];
+        }
+    };
 }
--- MemorablePlaces/app/src/main/java/com/example/memorableplaces/MapsActivity.java
+++ MemorablePlaces/app/src/main/java/com/example/memorableplaces/MapsActivity.java
@@ -14,6 +14,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,20 +23,36 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.SimpleTimeZone;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
-    private MarkerInfo markerInfo;
-    private String title;
-    private LatLng coordinates;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    MarkerInfo markerInfo;
+    LatLng userCoordinate;
+    Marker userMarker;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +60,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         Intent intent = getIntent();
-        markerInfo = (MarkerInfo) intent.getSerializableExtra("makerInfo");
+        markerInfo = intent.getParcelableExtra("MarkerInfo");
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -55,25 +72,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Set passed marker if it is not null
+        if(markerInfo.getCoordinates() != null) {
+            mMap.addMarker(new MarkerOptions().position(markerInfo.getCoordinates())
+                    .title(markerInfo.getTitle())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerInfo.getCoordinates(), 14));
+        }
+
+        // COMPLETE - Set listener for long clicks on map
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+                String newTitle = "";
                 Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
                 try {
                     List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
                     if(addresses != null && addresses.size() > 0) {
-                        Log.i("Address", addresses.get(0).toString());
+                        if(addresses.get(0).getThoroughfare() != null) {
+                            if(addresses.get(0).getSubThoroughfare() != null) {
+                                newTitle += addresses.get(0).getSubThoroughfare() + " ";
+                            }
+                            newTitle += addresses.get(0).getThoroughfare();
+                        }
                     }
-                    mMap.addMarker(new MarkerOptions().position(latLng).title("").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                if(newTitle.equals("")) {
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
+                    Date date = new Date();
+                    newTitle = formatter.format(date);
+                }
+
+                mMap.addMarker(new MarkerOptions().position(latLng)
+                        .title(newTitle)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                MarkerInfo newMarkerInfo = new MarkerInfo(newTitle, latLng);
+                MainActivity.markerTitles.add(newTitle);
+                MainActivity.markers.add(newMarkerInfo);
+                MainActivity.placesAdapter.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(), "Location saved!", Toast.LENGTH_SHORT).show();
             }
         });
 
-        title = markerInfo.title;
-        coordinates = new LatLng(markerInfo.location.getLatitude(), markerInfo.location.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(coordinates).title(title));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        // Setting listener for location changes
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                userCoordinate = new LatLng(location.getLatitude(), location.getLongitude());
+                if(userMarker == null) {
+                    userMarker = mMap.addMarker(new MarkerOptions().position(userCoordinate).title("You are here!"));
+                    if(markerInfo == null) mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCoordinate, 14));
+                }
+                else {
+                    userMarker.remove();
+                    userMarker = mMap.addMarker(new MarkerOptions().position(userCoordinate).title("You are here!"));
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+        };
+
+        // Setting location permissions
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastKnownLocation != null) {
+                userCoordinate = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                userMarker = mMap.addMarker(new MarkerOptions().position(userCoordinate).title("You are here!"));
+                if(markerInfo.getCoordinates() == null) mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCoordinate, 14));
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
     }
 }
